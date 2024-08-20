89b4869457ebf5b652c726f78a9982de05ff9ec2
h00ns <jihoon.hwang@imweb.me>
2024-08-20 20:04:15 +0900

    feat : Colors mdx

--- packages/components/src/Checkbox/Checkbox.Wrap.tsx
+++ packages/components/src/Checkbox/Checkbox.Wrap.tsx
@@ -1,23 +1,16 @@
 import { ForwardedRef, forwardRef } from 'react';
 import { CheckboxWrapProps } from './Checkbox.types';
-import { css } from '@emotion/react';
 import { token } from '@hoon-ds/token';
+import { Flex } from '../util';
 
 const _CheckboxWrap = (
   { children, ...htmlDivProps }: CheckboxWrapProps,
   ref: ForwardedRef<HTMLDivElement>,
 ) => {
   return (
-    <div
-      ref={ref}
-      css={css({
-        display: 'flex',
-        flexDirection: 'column',
-        rowGap: token.space[2],
-      })}
-      {...htmlDivProps}>
+    <Flex.Column gap={token.space[2]} ref={ref} {...htmlDivProps}>
       {children}
-    </div>
+    </Flex.Column>
   );
 };
 
